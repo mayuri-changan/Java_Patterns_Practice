5fc36d58d6b4c657f89db60dd5ebecfe9eb87fa3
mayuri-changan <mayuri2001changan@gmail.com>
2026-07-29 23:05:29 +0530

    Five_Star

---
+++ Five_Star.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class Five_Star {
+
+	public static void main(String[] args)
+	{
+	for(int m=0;m<=7;m++)
+	{
+	for(int n=0;n<=5;n++)
+	{
+	System.out.print("@ ");
+	}
+	System.out.println();
+
+	}
+
+	}
+	}
